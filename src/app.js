90be6dda7ae9ae8d7128bf2e20c2a30786f470f4
rashidh003 <get.rashidh@gmail.com>
2023-10-01 10:06:26 +0530

    initial changes1

---
+++ src/app.js
@@ -0,0 +1,19 @@
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+function typeWriting() {
+    const element = document.querySelector('.type');
+    if (isInViewport(element)) {
+        element.classList.add('typewriter');
+        window.removeEventListener('scroll', typeWriting);
+    }
+}
+
+window.addEventListener('scroll', typeWriting);
